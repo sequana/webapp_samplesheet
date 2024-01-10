066e5d029c94e11c29e70cf0844ee32ff87da9b8
Thomas Cokelaer <cokelaer@gmail.com>
2024-01-11 00:18:18 +0100

    add logo, remove previous deps

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 
 import streamlit as st
 import streamlit.components.v1 as stc
-from streamlit_extras.app_logo import add_logo
 
 import pandas as pd # installed with sequana
 from sequana.iem import IEM
@@ -12,21 +11,11 @@ st.set_page_config(
     page_title="Sample Sheet Validator",
     page_icon="imgs/logo_256x256.png",
     layout="wide",
-    #initial_sidebar_state="expanded",
-    #menu_items={
-    #    "Get help": "https://github.com/",
-    #    "Report a bug": "https://github.com/",
-    #    "About": """
-    #        ## Streamly Streamlit Assistant
-    #        
-    #        **GitHub**: https://github.com/sequana/st_sample_sheet
-    #
-    #    """
-    #}
 )
 
 def main():
-    add_logo("imgs/logo_256x256.png", height="150")
+    st.sidebar.write("Provided by the [Sequana teams](https://github.com/sequana/sequana)")
+    st.sidebar.image("imgs/logo_256x256.png")
     st.title("Sample Sheet validator")
 
     menu = ["Sample Sheet Validation (Illumina)","About"]
@@ -35,6 +24,21 @@ def main():
     if choice == "Sample Sheet Validation (Illumina)":
         st.subheader("Illumina case")
         data_file = st.file_uploader("Upload CSV",type=['csv'])
+
+        st.markdown("""
+        This validator checks for
+        - presence of ; at the end of lines indicated an edition with excel that
+              wrongly transformed the data into a pure CSV file
+        - inconsistent numbers of columns in the [DATA] section, which must be
+              CSV-like section
+        - Extra lines at the end are ignored
+        - special characters are forbidden except - and _
+        - for Sample_ID column uniqueness
+        - for index uniqueness (if single index)
+        - for combo of dual indices uniqueness
+        - that sample names are unique
+        """)
+
         if st.button("Process"):
             if data_file is not None:
                 file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
@@ -47,27 +51,22 @@ def main():
                     fout.write(data)
                 iem = IEM(filename)
 
-                
                 try:
                     st.write(f"This sample sheet contains {len(iem.df)} samples")
-                    st.write(iem.df)
                     iem.validate()
                 except SystemExit as err:
+                    st.write("An error was found :sob: See message below")
                     st.write(err)
                 else:
-                    st.write("OK")
+                    st.write(":champagne: Sample Sheet looks correct.")
+                finally:
+                    st.write("The data section looks like:")
+                    st.write(iem.df)
     else:
         st.subheader("About")
         st.info("Built with Streamlit")
         st.info("Thomas Cokelaer")
         st.text("Institut Pasteur, Paris, France")
-
-try:
-    from state import count_sessions
-    c = count_sessions()
-    st.write(c)
-except Exception as err:
-    print(err)
 
 
 if __name__ == '__main__':
