499188399f8a6da5e9f8ff4890370a75d35e6694
Thomas Cokelaer <cokelaer@gmail.com>
2024-01-10 11:21:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import pandas as pd
 from PIL import Image 
-
+import os
+from pathlib import Path
 
 def main():
     st.title("Sample Sheet validator")
@@ -10,30 +11,20 @@ def main():
     menu = ["Sample Sheet Validation (Illumina)","About"]
     choice = st.sidebar.selectbox("Menu",menu)
 
-    if choice == "Home":
-        st.subheader("Home")
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-            # To See Details
-            # st.write(type(image_file))
-            # st.write(dir(image_file))
-            file_details = {"Filename":image_file.name,"FileType":image_file.type,"FileSize":image_file.size}
-            st.write(file_details)
-
-            img = load_image(image_file)
-            st.image(img,width=250)
-
-
-    elif choice == "Sample Sheet Validation (Illumina)":
-        st.subheader("Sample Sheet Validation (Illumina)")
+    if choice == "Sample Sheet Validation (Illumina)":
+        st.subheader("Illumina case")
         data_file = st.file_uploader("Upload CSV",type=['csv'])
         if st.button("Process"):
             if data_file is not None:
                 file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
                 st.write(file_details)
 
+                st.write(data_file)
+                st.write(data_file.name)
+                
+                
                 from sequana.iem import IEM
-                iem = IEM(data_file.name)
+                iem = IEM(Path(data_file))
                 try:
                     st.write(f"This sample sheet contains {len(iem.df)} samples")
                     iem.validate()
