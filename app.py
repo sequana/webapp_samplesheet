c3f412c3618fd260b19af868539c4adf88ba3ffe
Thomas Cokelaer <cokelaer@gmail.com>
2024-02-03 21:12:42 +0100

    Update menu

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
-from pathlib import Path
 import os
 import time
 from collections import defaultdict
+from pathlib import Path
 
+import pandas as pd  # installed with sequana
+import requests
 import streamlit as st
-import streamlit.components.v1 as stc
-
-
-import pandas as pd # installed with sequana
+from streamlit_option_menu import option_menu
 from sequana.iem import IEM
 from sequana.rnadiff import RNADesign
 
+
 st.set_page_config(
-    page_title="Sample Sheet Validator",
+    page_title="Sample Sheet and Design Validator",
     page_icon="imgs/logo_256x256.png",
     layout="wide",
+    menu_items={"Report a bug": "https://github.com/sequana/st_sample_sheet/issues/new/choose"},
 )
 
 
 def main():
     st.sidebar.write("Provided by the [Sequana teams](https://github.com/sequana/sequana)")
     st.sidebar.image("imgs/logo_256x256.png")
-    st.title("Sample Sheet validator")
+    st.title("Sample Sheet and Design Validator")
 
-    menu = ["Sample Sheet Validation (Illumina)", "RNAdiff Design File (Sequana)", "About"]
-    choice = st.sidebar.selectbox("Menu",menu)
+    menu = ["Sample Sheet Validation (Illumina)", "RNAdiff Design File (Sequana)", "Examples", "About"]
+
+    # 1. as sidebar menu
+    with st.sidebar:
+        choice = option_menu(
+            "Main Menu", menu, icons=["gear", "gear", "cloud-upload", ""], menu_icon="cast", default_index=1
+        )
 
     if choice == "Sample Sheet Validation (Illumina)":
+        st.markdown(
+            "Please select a validator from the left hand side menu (default is Illumina Sample Sheet). Valid examples are available [here](https://github.com/sequana/st_sample_sheet/)"
+        )
         st.subheader("Illumina case", divider="blue")
 
         data_file = st.file_uploader(
-            ("Drop a valid sample sheet here below and press the **Process** button. "
-            'Valid examples are available [here](https://github.com/sequana/st_sample_sheet/)'),
-            type=['csv', 'txt'])
-
+            "Drop a sample sheet here below and press the **Process** button. ", type=["csv", "txt"]
+        )
 
         if st.button("Process"):
             if data_file is not None:
-                file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-                #st.write(file_details)
+                file_details = {"Filename": data_file.name, "FileType": data_file.type, "FileSize": data_file.size}
+                # st.write(file_details)
 
                 # read to save locally
                 data = data_file.read().decode()
@@ -48,15 +55,15 @@ def main():
                 iem = IEM(filename)
 
                 try:
-                    #st.write(f"This sample sheet contains {len(iem.df)} samples")
+                    # st.write(f"This sample sheet contains {len(iem.df)} samples")
                     iem.validate()
                 except SystemExit as err:
-                    st.header("Validation Results", divider='blue')
+                    st.header("Validation Results", divider="blue")
                     msg = "Error(s) found. :sob: See message below from Sequana"
                     st.error(msg)
                     st.info(err)
                 else:
-                    st.header("Validation Results", divider='blue')
+                    st.header("Validation Results", divider="blue")
                     # emoji within div do not seem to work
                     msg = ":champagne: Sample Sheet looks correct. :champagne:"
                     st.success(msg)
@@ -65,27 +72,22 @@ def main():
                 checks = iem.checker()
 
                 msgs = defaultdict(list)
-                emoji = {
-                    "Error": ":x:",
-                    "Success": ":white_check_mark:",
-                    "Warning": ":warning:"
-                }
+                emoji = {"Error": ":x:", "Success": ":white_check_mark:", "Warning": ":warning:"}
 
                 bar = st.progress(0)
-                for i,check in enumerate(checks):
-
-                    status = check['status']
-                    msg = check['msg']
+                for i, check in enumerate(checks):
+                    status = check["status"]
+                    msg = check["msg"]
 
                     msgs[status].append(f"{status} {emoji[status]}. {msg}\n\n")
-                    bar.progress((i + 1) / len(checks) )
+                    bar.progress((i + 1) / len(checks))
                     time.sleep(0.2)
 
                 for error in msgs["Error"]:
                     st.error(error)
                 for warning in msgs["Warning"]:
                     st.warning(warning)
-                for success in msgs['Success']:
+                for success in msgs["Success"]:
                     st.success(success)
 
                 # =============================================================== original file
@@ -94,19 +96,21 @@ def main():
                 st.code(open(filename).read())
 
                 # =============================================================== corrected file
-                if len(msgs['Error']):
+                if len(msgs["Error"]):
                     st.subheader("Corrected file", divider="blue")
-                    st.caption("Quick fix here below gets rid of extra trailing ; . Other types of errors are difficult to correct automatically. You will need to correct the file manually. We strongly recommend you to use IEM software (from Illumina) for that. Otherwise, to quickly edit the file, do not use Excel because the sample sheet is not a CSV file despite the fact that the extension is usually .csv   ")
+                    st.caption(
+                        "Quick fix here below gets rid of extra trailing ; . Other types of errors are difficult to correct automatically. You will need to correct the file manually. We strongly recommend you to use IEM software (from Illumina) for that. Otherwise, to quickly edit the file, do not use Excel because the sample sheet is not a CSV file despite the fact that the extension is usually .csv   "
+                    )
 
                     iem.quick_fix("test.txt")
                     data = open("test.txt", "r").read()
                     st.download_button(
                         label="Download data as CSV",
                         data=data,
-                        file_name='sample.csv',
-                        mime='text/csv',
+                        file_name="sample.csv",
+                        mime="text/csv",
                     )
-                    #st.code(open(filename).read())
+                    # st.code(open(filename).read())
 
                 # =============================================================== data section
                 st.subheader("Data section", divider="blue")
@@ -114,17 +118,22 @@ def main():
                 df = iem.df.copy()
                 st.write(df)
     elif choice == "RNAdiff Design File (Sequana)":
+        st.markdown(
+            "Please select a validator from the left hand side menu (default is Illumina Sample Sheet). Valid examples are available [here](https://github.com/sequana/st_sample_sheet/)"
+        )
         st.subheader("Sequana RNAdiff design file", divider="blue")
 
         data_file = st.file_uploader(
-            ("Drop a RNAdiff design file here below and press the **Process** button. "
-            'Valid examples are available [here](https://github.com/sequana/st_sample_sheet/)'),
-            type=['csv', 'txt'])
-
+            (
+                "Drop a RNAdiff design file here below and press the **Process** button. "
+                "Valid examples are available [here](https://github.com/sequana/st_sample_sheet/)"
+            ),
+            type=["csv", "txt"],
+        )
 
         if st.button("Process"):
             if data_file is not None:
-                file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+                file_details = {"Filename": data_file.name, "FileType": data_file.type, "FileSize": data_file.size}
 
                 # read to save locally
                 data = data_file.read().decode()
@@ -135,18 +144,26 @@ def main():
                     design = RNADesign(filename)
                     st.write("ok")
                 except SystemExit as err:
-                    st.header("Validation Results", divider='blue')
+                    st.header("Validation Results", divider="blue")
                     msg = "Error(s) found. :sob: See message below from Sequana"
                     st.error(msg)
                     st.error(err)
+    elif choice == "Examples":
+        st.subheader("Illumina")
+        url = "https://raw.githubusercontent.com/sequana/st_sample_sheet/main/sample_sheet.csv"
+        r = requests.get(url, allow_redirects=True)
+        data = r.content.decode()
+        st.code(data, language="bash")
 
+        st.subheader("RNAdiff design file")
 
     else:
         st.subheader("About")
-        st.info("Built with Streamlit")
-        st.info("Thomas Cokelaer")
-        st.text("Institut Pasteur, Paris, France")
+        st.markdown(
+            "This application is part of the [Sequana Project](https://github.com/sequana), which is dedicated to NGS analysis. Please see the [online documentation](https://sequana.readthdocs.io) as well as https://sequana.github.io for more information; the code used in this application is based on the [IEM module](https://github.com/sequana/sequana) of the Sequana Python library"
+        )
+        st.info("Application Author: Thomas Cokelaer")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
